28d526acda60fd37ed7fa6e6f208f0be3302aca9
callumfairbro <callum.fairbrother11@gmail.com>
2022-08-16 23:13:37 +0100

    Testing webhooks

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -714,8 +714,6 @@ $settings['trusted_host_patterns'] = array(
   '^www\.neontank\.co\.uk$\.web$',
 );
 
-// Another test
-
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
